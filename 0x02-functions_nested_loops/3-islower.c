4f3d35910c44a1914e3357241a35aada98d15a63
Mina2423 <amina.mansouri268@gmail.com>
2023-03-22 18:35:48 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase
- * check character c
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - checker
- * the character to be checked c
+ * isalpha - checks for alphabetic character
+ * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
