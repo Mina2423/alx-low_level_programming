5d2b4c24570f0eea15dfc8a2c7f9d42920ba295a
Mina2423 <amina.mansouri268@gmail.com>
2023-04-17 18:40:51 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,8 +4,8 @@
 /**
  * struct dog - a dog's basic info
  * @name: First member
- * @age: Second mamber
- * @owner: Third mamber
+ * @age: Second member
+ * @owner: Third member
  *
  * Description: Longer description
  */
@@ -21,7 +21,7 @@ struct dog
  */
 typedef struct dog dog_t;
 
-void int_dog(struct dog *d, char *name, dloat age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
